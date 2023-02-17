f9b5b764ab616daa03b8a57ae699d0960efae42f
arialghifari <arialghifari99@gmail.com>
2023-02-18 07:28:56 +0800

    feat: show all guesses

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,18 +1,17 @@
 import React from 'react';
 
-function GuessInput() {
-  const [guess, setGuess] = React.useState('');
+function GuessInput({ handleSubmitGuess }) {
+  const [tentativeGuess, setTentativeGuess] = React.useState('');
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log({ guess });
-    setGuess('');
+    handleSubmitGuess(tentativeGuess);
+    setTentativeGuess('');
   }
 
   function handleOnChange(event) {
-    if (event.target.value.length <= 5) {
-      return setGuess(event.target.value.toUpperCase());
-    }
+    if (event.target.value.length > 5) return;
+    setTentativeGuess(event.target.value.toUpperCase());
   }
 
   return (
@@ -21,7 +20,7 @@ function GuessInput() {
       <input
         id='guess-input'
         type='text'
-        value={guess}
+        value={tentativeGuess}
         onChange={handleOnChange}
         pattern='[A-Za-z]{5}'
         title='Guesses have to contain 5-letter words.'
