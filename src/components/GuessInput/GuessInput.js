d2c831b51be79d3b833338cf9b40195a9ad0ec8d
arialghifari <arialghifari99@gmail.com>
2023-08-02 00:28:50 +0800

    feat: GuessInput

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState('')
+
+  function handleOnSubmit(e) {
+    e.preventDefault()
+
+    if (guess.length !== 5) return
+
+    console.log({ guess })
+    setGuess('')
+  }
+
+  return (
+    <form onSubmit={handleOnSubmit} className="guess-input-wrapper">
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        required
+        value={guess}
+        onChange={(e) => setGuess(e.target.value.toUpperCase())}
+        maxLength={5}
+        minLength={5}
+        pattern="[a-zA-Z]{5}"
+        title="must be exact 5 letters words"
+        autoComplete="off"
+      />
+    </form>
+  )
+}
+
+export default GuessInput
